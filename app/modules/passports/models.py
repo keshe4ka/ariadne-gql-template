5a836a0ce3f4f263a315023842859e7f28abd3c5
keshe4ka <a.anaschenko@btc-s.ru>
2023-07-18 15:10:59 +0300

    feat(passports): add index

--- app/modules/passports/models.py
+++ app/modules/passports/models.py
@@ -14,7 +14,7 @@ class Passport(BaseMixin):
     __tablename__ = 'passports'
 
     id = Column(Integer, primary_key=True)
-    number = Column(Integer, nullable=False, unique=True)
+    number = Column(Integer, nullable=False, index=True, unique=True)
     country = Column(String, nullable=False)
     date_of_issue = Column(Date, nullable=False)
     type = Column(Enum(PassportType), nullable=False)
